bf3b6597f2ff03b54cc11c34d7dda88da0d2c482
Frits Hoogland <fhoogland@yugabyte.com>
2022-03-18 15:30:21 +0100

    set external scripts to perform run again

--- runfile.sql
+++ runfile.sql
@@ -1,3 +1,3 @@
 \set ON_ERROR_STOP true
-call ybio.run(1, 1, :tag);
---call ybio.setup(1, :schema, :tag);
+call ybio.run(:config, 1, :tag);
+--call ybio.setup(:config, :schema, :tag);
--- runner.sh
+++ runner.sh
@@ -1,13 +1,16 @@
 FACTOR=$1
-TAG=$(echo -e "\x27$2\x27")
-YSQL_HOSTS="10.9.112.14 10.9.207.252 10.9.140.59"
+TAG_NAME=$2
+YSQL_HOSTS="192.168.66.80 192.168.66.81 192.168.66.82"
+YBIO_CONFIG=1
 USER=yugabyte
 #SCHEMA_NR_COUNTER=1
 for HOST in $YSQL_HOSTS; do
   for NR in $(seq $FACTOR); do
     #SCHEMA=$(echo  -e "\x27$SCHEMA_NR_COUNTER\x27")
-    ysqlsh -h $HOST -p 5433 -U $USER -v tag="$TAG" -f runfile.sql >& run.output.$HOST.$NR.txt &
-    #ysqlsh -h $HOST -p 5433 -U $USER -v tag="$TAG" schema="$SCHEMA" -f runfile.sql >& run.output.$HOST.$NR.txt &
+    CONFIG=$(echo  -e "\x27$YBIO_CONFIG\x27")
+    TAG=$(echo -e "\x27$TAG_NAME\x27")
+    ysqlsh -h $HOST -p 5433 -U $USER -v tag="$TAG" -v config="$CONFIG" -f runfile.sql >& run.output.$HOST.$NR.txt &
+    #ysqlsh -h $HOST -p 5433 -U $USER -v tag="$TAG" -v schema="$SCHEMA" -v config="$CONFIG" -f runfile.sql >& run.output.$HOST.$NR.txt &
     #let SCHEMA_NR_COUNTER++
   done
 done
